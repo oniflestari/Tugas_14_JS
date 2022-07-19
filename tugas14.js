5241d0579ced97af10292e059c65e3a02ac8a73b
oniflestari <onifahmilestari@gmail.com>
2022-07-19 12:56:07 +0700

    Tugas 14 JS
    Tugas ini saya kumpulkan untuk Niomic

---
+++ tugas14.js
@@ -0,0 +1,7 @@
+var a = 37;
+var b = 22;
+var hasil = a / b;
+console.log('hasil :', hasil)
+console.log('ceil :', Math.ceil(hasil))
+console.log('floor :', Math.floor(hasil))
+console.log('round :', Math.round(hasil))
